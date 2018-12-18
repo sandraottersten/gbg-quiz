0376e0947815e7bbcfccdf3523adee90b3ba027e
Christian <32764514+neowara@users.noreply.github.com>
2018-12-18 07:32:47 +0100

    Working easy bot

--- src/views/PlayGame.vue
+++ src/views/PlayGame.vue
@@ -2,11 +2,12 @@
   <div id="app">
     <h3>Question</h3>
     <h1>{{questions[number].question}}</h1>
-    <input  @input="newValue" type="number" onfocus="this.value=''" v-on:keypress = "OnlyNumbers"/>       
-    <button @click="makeGuess">Make a guess</button>
-    <span id="errormess" style="color: orangered; display: none"><br><br>* Endast siffror! </span>
+    <input ref="my_input" @input="newValue" type="number" onfocus="this.value=''" v-on:keypress = "OnlyNumbers"/>       
+    <button @click="easyBot">Make a guess</button>
+    <span id="errormess" style="color: orangered; display: none"><br><br>* Only numbers! </span>
+    <p id="guess" style="display: none"><br>Guess a {{highLow}} number!</p>
     <Timer />
-    <p>My guess: {{value}}</p>
+    <p>My guess: {{userGuess}}</p>
     <p>Bot guess: {{bot}}</p>
   </div>
 </template>
@@ -19,7 +20,9 @@ export default {
   name: 'PlayGame',
   data: function () {
     return {
-      numberKeys: []
+      numberKeys: [],
+      highLow: '',
+      userGuess: 0
     }
   },
 
@@ -39,7 +42,7 @@ export default {
       },
       bot() {
         return this.$store.state.bot;
-      }
+      },
       },
 
   methods: {
@@ -49,22 +52,46 @@ export default {
       randNum: function(){
         this.num = Math.floor(Math.this.questions.length);
       },
-      ranNumBot() {
-        this.$store.state.bot = Math.floor(Math.random() * 100) + 1;
+      ranNumBot(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        this.$store.state.bot = Math.floor(Math.random() * (max - min)) + min;
       },
       makeGuess(value, number, bot) {
         if(this.value < this.questions[this.number].answer){
-          alert("högre");
-        this.ranNumBot();
+          this.getFormValues();
+          this.highLow = 'higher';
+          document.getElementById("guess").style.display = "inline";
         }
         else if (this.value > this.questions[this.number].answer){
-          alert("lägre");
-          this.$store.state.bot = Math.floor(Math.random() * 100) + 1;
+          this.getFormValues();
+          this.highLow = 'lower';
+          document.getElementById("guess").style.display = "inline";
         }
         else {
+          this.getFormValues();
           alert("rätt");
         }
       },
+      getFormValues() {
+        this.userGuess = this.$refs.my_input.value
+      },
+      easyBot(number, bot) {
+        console.log(this.bot + " Current guess")
+        if (this.bot < this.questions[this.number].answer) {
+          this.ranNumBot(0, 100);
+          console.log(this.bot + " Guess higher");
+      }
+        else if (this.bot > this.questions[this.number].answer) {
+          this.ranNumBot(10, 80);
+          console.log(this.bot + " Guess lower");
+      } 
+        else {
+          alert("Bot wins this round!");
+          console.log(this.bot + " Correct guess, good bot!")
+          }
+      },
+      
       OnlyNumbers(e) {
         var keyCode = e.which;
         var ret = ((keyCode >= 48 && keyCode <= 57) || this.numberKeys.indexOf(keyCode) != -1);
