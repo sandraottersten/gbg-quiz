4df1137bd993c3e0663a3f5356404757896d2338
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2018-12-19 12:14:17 +0100

    Made login site

---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="login">
+    <h3>Sign In</h3>
+    <input type="text" placeholder="Email">
+    <br>
+    <input type="text" placeholder="Password">
+    <br>
+    <button>Connection</button>
+    <p>You don't have an account yet? You can create one</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "login",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<!--Style this specific site -->
+<style scoped>
+</style>
