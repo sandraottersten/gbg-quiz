36c810678208294ac605e9b148b77d6e546290f2
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2018-12-20 09:21:28 +0100

    cleaned code

--- src/router.js
+++ src/router.js
@@ -56,28 +56,16 @@ const router = new Router({
 router.beforeEach((to, from, next) => {
   var requiresAuth = to.matched.some(record => record.meta.requiresAuth);
   var currentUser = firebase.auth().currentUser;
-  // when route requires auth and there's no current user, reidrect to '/login'
+
   if (requiresAuth && !currentUser) {
     next('/login');
-    // when we go to login route and are already logged in, we can skip this page
-    // so we redirect to the homepage
+
   } else if (to.path == '/login' && currentUser) {
     next('/');
-    // if none of the above matches, we have a normal navigation that should just go through
-    // so we call `next()`
+
   } else {
-    next(); // you called `next('/')` which redirected to the homepage over and over again.
+    next(); 
   }
 });
 
-/*
-router.beforeEach((to, from, next) => {
-  // to and from are both route objects
-  const currentUser = firebase.auth().currentUser;
-  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
-  if (requiresAuth && !currentUser) next('login');
-  else if (!requiresAuth && currentUser) next('home');
-  else next();
-});
-*/
 export default router;
