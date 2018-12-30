e4c78b0cda6ac5d123e2e21d1aff3111e061f03b
Åsa H <asa.hildingsson@hotmail.com>
2018-12-30 13:17:07 +0100

    Design
    New design with blue Gbg theme

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>The GBG Game</h2>
+    <p>The Great Big Game about Gbg</p>
+    <router-link to="/gamemenu"><button class="startgamebutton"> <span>Go for it </span></button></router-link>
+      
+ <br><br>
+      
+    <audio controls>
+  <source src="http://k007.kiwi6.com/hotlink/3h77ek6oz8/gbgsong.mp3" type="audio/mpeg">
+</audio>  
+      
+      
+  </div>
+</template>
+
+
---
+++ src/views/SignUp.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="sign-up">
+    <p>Let's create you a new account!</p>
+    <input type="text" v-model="email" placeholder="Email">
+    <br>
+    <input type="password" v-model="password" placeholder="Password">
+    <br>
+    <button class="buttonsubmit" @click="signUp">Sign Up</button>
+    <br>
+    <span>or go back to
+      <router-link to="/login">login</router-link>.
+    </span>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase";
+
+export default {
+  name: "signUp",
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    signUp: function() {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.email, this.password)
+        .then(
+          function(user) {
+            this.$router.replace("home");
+          },
+          function(err) {
+            alert("Whoops.." + err.message);
+          }
+        );
+    }
+  }
+};
+</script>
