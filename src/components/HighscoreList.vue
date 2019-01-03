7aa0cf95bd7540648343b15b4035914bf1e4df88
Sandra <otterstensandra@gmail.com>
2019-01-03 10:33:12 +0100

    changed gitignore

---
+++ src/components/HighscoreList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>All time scorer</h1>
+        <ul>
+            <li v-for="point in allUsers" :key="point.id">
+                {{point}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import {db} from '../firebase-config'
+
+export default {
+    name: 'HighscoreList',
+    data () {
+        return {
+            point: ''
+        }
+    },
+    created () {
+        this.$bindAsArray('allUsers', db.ref('allUsers'))
+    },
+    firebase: {
+        allUsers: db.ref('allUsers')
+    }
+}
+</script>
