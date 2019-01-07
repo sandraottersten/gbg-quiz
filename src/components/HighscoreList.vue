1c9259af3b93045e6a11299d2d551de5759d0885
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-01-07 18:51:01 +0100

    high score list is done

--- src/components/HighscoreList.vue
+++ src/components/HighscoreList.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>All time scorer</h1>
         <ol>
-            <li v-for="point in allUsers" :key="point.id">
+            <li v-for="point in allUsers.slice().reverse()" :key="point.id">
                 {{point.name}} {{point.newPoint}}
             </li>
         </ol>
