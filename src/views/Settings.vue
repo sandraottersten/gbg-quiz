9d201a4e0147e01a9f72814f1cdef6cc2a56b8f3
Vendela <vendelapatricia@hotmail.com>
2019-01-07 17:42:29 +0100

    numOfGuesses fixed

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -6,7 +6,6 @@
         <button class="gamebutton" @click="afunction('trams')">Trams</button>
         <button class="gamebutton" @click="afunction('history')">History</button>
         <button class="gamebutton" @click="afunction('food')">Food</button>
-        <button class="gamebutton" @click="allCat()">All</button>
       </div>
       <router-link to="/playgame"><button class="gamebutton">Start Game</button></router-link>
       <button class="gamebutton" @click="logout">Logout</button>
@@ -41,20 +40,9 @@ export default {
       var num = Math.floor(Math.random() * arr.length);
       this.$store.state.theQuestion = arr[num].question;
       this.$store.state.theAnswer = arr[num].answer;
-    },
-    allCat: function() {
-      var array = [];
-       for (var i = 0; i < this.questions.length; i++){
-         array.push(this.questions[i]);
-         var num = Math.floor(Math.random() * array.length);
-       }
-       console.log(array[num].question);
-    },
-
-    randNum: function(){
-      this.$store.state.number = Math.floor(Math.random() * this.questions.length);
       this.$store.state.numOfGuesses = 0
     },
+    
     logout: function() {
       firebase
         .auth()
--- src/views/Winner.vue
+++ src/views/Winner.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-      
+
     <h2 v-show="winner">You won! <br>
     <img width="300px" v-for="img in images" v-bind:src="img"><br>
 </h2>
     <h2 v-show="!winner">You lost!</h2>
-    <p>Correct answer is: {{this.questions[this.number].answer}}</p>
-    <p>Number of guesses: {{this.$store.state.numOfGuesses}}</p>      
-      
-      
-  
+    <p>Correct answer is: {{this.$store.state.theAnswer}}</p>
+    <p>Number of guesses: {{this.$store.state.numOfGuesses}}</p>
+
+
+
     <router-link to="/settings"><button class="gamebutton">Play again</button></router-link>
     <button class="gamebutton" @click="logout">Logout</button>
   </div>
@@ -20,12 +20,12 @@ import firebase from "firebase";
 import {db} from '../firebase-config'
 
 export default {
-    
+
 data() {
     return {
         images: ['http://i67.tinypic.com/155mdzn.png']
     }
-    
+
 },
   name: "Winner",
   firebase: {
