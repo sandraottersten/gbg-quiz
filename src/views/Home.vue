5226fe4ad1c5e2436dee43570f81a9003bf01ef9
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-01-10 15:42:24 +0100

    fixed the names for the better

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
         <h2>The GBG Game</h2>
         <h3>How well do you really know your Gothenburg?</h3>
         <p>Test your skills here against our players: <br>
-          Easy-Glenn, Mediocre-Håkan or Hardcore-Mirijam.</p>
+          Glenn, Håkan or Mirijam.</p>
         <p id="musicText">And don't forget to first push play icon below for some music to get into the right mood!</p>
         <h4>Good luck!</h4>
 
