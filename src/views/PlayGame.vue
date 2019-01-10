20d64e028b858e9fd009004e53e59d04164a4239
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-01-10 10:11:45 +0100

    Update PlayGame.vue

--- src/views/PlayGame.vue
+++ src/views/PlayGame.vue
@@ -1,17 +1,16 @@
 <template>  
   <div id ="content">  
-    <h3>Question</h3>
     <h1>{{theQuestion}}</h1>
-    <flash-message class="myCustomClass"></flash-message>
     <input id="guess" @input="newValue" type="number" autofocus="this.value=''" v-on:keypress.enter = "makeGuess"  v-on:keypress = "OnlyNumbers"/>
     <button class="guessbutton" @click="makeGuess">Make a guess</button>
     <p id="errormess" style="color: orangered; display: none">Only numbers! </p>
-    <p id="playerTurn" v-show="playersTurn">It's the player's turn! </p>
-    <p id="botTurn" v-show="!playersTurn">It's the bot's turn! </p>
-    <flash-message class="myCustomClass"></flash-message>
+    <p id="playerTurn" v-show="playersTurn">Your turn! </p>
+    <p id="botTurn" v-show="!playersTurn">Bot turn! </p>
     <Timer v-show="show" ref="form"/>
     <p>My guess: {{value}} </p>
     <p>Bot guess: {{bot}} </p>
+    <flash-message class="myCustomClass"></flash-message>
+    <br>
   </div>
 </template>
 
